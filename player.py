5da81a1dc466b8cf0f68bf05ef18cb5f8fbbc414
Rahul Raju <rahulraju3107@gmail.com>
2025-09-20 19:27:01 +1000

    Cleaned initialisation variables

--- player.py
+++ player.py
@@ -7,7 +7,6 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
-        pygame.sprite.Sprite.__init__(self, self.containers)
     
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
