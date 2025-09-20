d6cb6af23b4f68486ae3f001e3cbe7377f1450fb
Rahul Raju <rahulraju3107@gmail.com>
2025-09-20 14:19:29 +1000

    Implemented Player class and draw to screen

--- main.py
+++ main.py
@@ -1,29 +1,34 @@
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
     pygame.init()
-    running = True
-    
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    running = True
     dt = 0
+    black = (0, 0, 0)
 
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    black = (0, 0, 0)
 
     # Game loop
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+
         screen.fill(color=black)
-        dt = clock.tick(60) / 1000
+        player.draw(screen)
         pygame.display.flip()
+
+        # Limit framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+
     pygame.quit()
 
 if __name__ == "__main__":
