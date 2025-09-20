f3ce657366776263932cea9be3f01549130201ab
Rahul Raju <rahulraju3107@gmail.com>
2025-09-20 12:52:52 +1000

    Created clock for 60 FPS

--- main.py
+++ main.py
@@ -1,25 +1,30 @@
 import pygame
 from constants import *
 
+
 def main():
     pygame.init()
+    running = True
+    
+    clock = pygame.time.Clock()
+    dt = 0
+
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
-   
     black = (0, 0, 0)
 
-    # Infinite while loop
-    while True:
+    # Game loop
+    while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                break
+                running = False
         screen.fill(color=black)
+        dt = clock.tick(60) / 1000
         pygame.display.flip()
-
+    pygame.quit()
 
 if __name__ == "__main__":
     main()
